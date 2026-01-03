44b1c110ee9428bd2538dc9017ee2cf4770790c0
mfaruqi35 <muhammad.faruqi35@gmail.com>
2026-01-03 16:47:19 +0700

    create navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import {
+  Instagram,
+  Linkedin,
+  Mail,
+  Check,
+  Copy,
+  ChevronRight,
+  ExternalLink,
+} from "lucide-react";
+
+export default function Navbar() {}
