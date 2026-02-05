c0b5942e72b913b5aa1034c8e25f9fda004aef6e
mfaruqi35 <muhammad.faruqi35@gmail.com>
2026-02-05 22:58:32 +0700

    fix nav color and add rotating text

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,17 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { X, ArrowUpRight } from "lucide-react";
+import { Plus_Jakarta_Sans, Inter } from "next/font/google";
+
+const logoFont = Inter({
+  subsets: ["latin"],
+  weight: "700",
+});
+
+const navFont = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: "600",
+});
 
 export default function Navbar() {
   const [activeSection, setActiveSection] = useState("hero");
@@ -39,25 +50,44 @@ export default function Navbar() {
     <nav className="fixed top-0 w-full z-50 bg-black md:bg-transparent h-14 flex items-center justify-between px-6 md:pl-10 text-white border-b border-slate-800 md:mix-blend-difference">
       <div>
         <Link href="/">
-          <div className="font-bold tracking-tighter">MUHAMMAD FARUQI</div>
+          <div className={`font-bold tracking-tighter ${logoFont.className}`}>
+            MUHAMMAD FARUQI
+          </div>
         </Link>
       </div>
 
       <div className="hidden md:flex flex-row h-full justify-between items-center w-3xl border-l border-slate-800 px-25 font-medium">
-        {navLinks.map((link) => (
-          <div key={link.id}>
+        {navLinks.map((link) => {
+          const isActive = activeSection === link.id;
+
+          return (
             <Link
+              key={link.id}
               href={link.href}
-              className={`transition-colors duration-300 ${
-                activeSection === link.id
-                  ? `text-white md:text-[#2b2b2b]`
-                  : `text-slate-500 hover:text-blue-600`
-              }`}
+              className={`group relative inline-block h-6 pb-1 text-white overflow-hidden ${navFont.className}`}
             >
-              {link.name}
+              <span
+                className={`block transition-all duration-300 ease-out
+        group-hover:-translate-y-full group-hover:-rotate-2
+        `}
+              >
+                {link.name}
+              </span>
+
+              <span
+                className="absolute inset-0 block translate-y-full rotate-3
+        transition-all duration-300 ease-out
+        group-hover:translate-y-0 group-hover:rotate-0 text-white"
+              >
+                {link.name}
+              </span>
+
+              {isActive && (
+                <span className="absolute left-1/2 -bottom-2 w-2 h-2 bg-black rounded-sm -translate-x-1/2" />
+              )}
             </Link>
-          </div>
-        ))}
+          );
+        })}
       </div>
 
       <button
